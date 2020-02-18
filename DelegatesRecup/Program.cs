b86a51a1e6e4f82ba97971d38ac18f5856a88f3b
OliverMengich <oliverkemei2000@gmail.com>
2020-02-18 20:25:05 +0300

    Recup on delegates

---
+++ DelegatesRecup/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegatesRecup
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+        public delegate void confDel();
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = new MyDelegate(Std1());
+            
+        }
+
+        private static MyDelegate Std1()
+        {
+            Console.WriteLine("Method from a delegate");
+            Thread.Sleep(10000);
+            return Std1();
+        }
+    }
+    class Std1
+    {
+        public Std1()
+        {
+            Console.WriteLine("Constructor");
+        }
+    }
+    class Std2
+    {
+
+    }
+}
